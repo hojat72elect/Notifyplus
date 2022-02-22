bafda48f5bb30f47dde28d94deff0d195af44249
mojan <hojat72elect@gmail.com>
2022-02-21 21:08:42 -0500

    Cleaned legacy code

--- app/src/main/java/ca/sudbury/hghasemi/notifyplus/FloatingViewService.kt
+++ app/src/main/java/ca/sudbury/hghasemi/notifyplus/FloatingViewService.kt
@@ -262,8 +262,6 @@ class FloatingViewService : Service()
             it?.setOnClickListener {
                 // Turn on/off the bluetooth
                 val btAdapter = (getSystemService(BLUETOOTH_SERVICE) as BluetoothManager).adapter
-
-
                 if (btAdapter == null) {
                     Toast.makeText(
                         this.applicationContext,
@@ -698,25 +696,3 @@ class FloatingViewService : Service()
 
 //
 
-
-/*
-* This is how you check for bluetooth permission
-*
-*  if (ActivityCompat.checkSelfPermission(
-                                this,
-                                Manifest.permission.BLUETOOTH_CONNECT
-                            ) != PackageManager.PERMISSION_GRANTED
-                        ) {
-                            // TODO: Consider calling
-                            //    ActivityCompat#requestPermissions
-                            // here to request the missing permissions, and then overriding
-                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                            //                                          int[] grantResults)
-                            // to handle the case where the user grants the permission. See the documentation
-                            // for ActivityCompat#requestPermissions for more details.
-                            return
-                        }
-*
-*
-*
-* */
